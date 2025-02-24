5cbb513a0e6a2b1c3ec803d420dae52ed308437d
unknown <andres.sepulveda-s@labinfo.is.escuelaing.edu.co>
2025-02-24 09:20:46 -0500

    Spring intialize

---
+++ src/main/java/eci/edu/cvds/pruebaCVDS/PruebaCvdsApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.cvds.pruebaCVDS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaCvdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaCvdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eci/edu/cvds/pruebaCVDS/PruebaCvdsApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.cvds.pruebaCVDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaCvdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
